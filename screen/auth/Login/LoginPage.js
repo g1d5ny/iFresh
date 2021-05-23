09d380bb44edb92cdc7f76b2c3d243e149078f3b
g1d5ny <jiwon936626@naver.com>
2021-05-24 01:18:14 +0900

    Login Page Edit

--- screen/auth/Login/LoginPage.js
+++ screen/auth/Login/LoginPage.js
@@ -1,128 +1,147 @@
-import React, {useEffect, useState} from 'react';
+import React, { useEffect, useState } from "react";
 import {
-    SafeAreaView,
-    StyleSheet,
-    View,
-    Text,
-    Image,
-    TouchableOpacity,
-    ScrollView,
-    Platform,
-    Alert, ImageBackground,
-} from 'react-native';
+  SafeAreaView,
+  StyleSheet,
+  View,
+  Text,
+  Image,
+  TextInput,
+  TouchableOpacity,
+  ScrollView,
+  Platform,
+  Alert, ImageBackground,
+} from "react-native";
 import {
-    KakaoOAuthToken,
-    KakaoProfile,
-    getProfile as getKakaoProfile,
-    login,
-    logout,
-    unlink,
-} from '@react-native-seoul/kakao-login';
+  KakaoOAuthToken,
+  KakaoProfile,
+  getProfile as getKakaoProfile,
+  login,
+  logout,
+  unlink,
+} from "@react-native-seoul/kakao-login";
+import { Style } from "../../../styles/user/Style";
+import { AuthStyle } from "../../../styles/auth/AuthStyle";
 
 const LoginPage = ({ navigation, route }) => {
 
-    const [result, setResult] = useState('');
+  const [result, setResult] = useState("");
 
-    const signInWithKakao = async() => {
-        const token = await login();
-        setResult(JSON.stringify(token));
-        navigation.navigate('TabNavigation')
-    };
+  const signInWithKakao = async () => {
+    const token = await login();
+    setResult(JSON.stringify(token));
+    navigation.navigate("TabNavigation");
+  };
 
-    const signOutWithKakao = async () => {
-        const message = await logout();
+  const signOutWithKakao = async () => {
+    const message = await logout();
 
-        setResult(message);
-    };
+    setResult(message);
+  };
 
-    const getProfile = async () => {
-        const profile = await getKakaoProfile();
+  const getProfile = async () => {
+    const profile = await getKakaoProfile();
 
-        setResult(JSON.stringify(profile));
-    };
+    setResult(JSON.stringify(profile));
+  };
 
-    const unlinkKakao = async (): Promise<void> => {
-        const message = await unlink();
+  const unlinkKakao = async (): Promise<void> => {
+    const message = await unlink();
 
-        setResult(message);
-    };
+    setResult(message);
+  };
 
-    return (
-        <SafeAreaView style={{flex: 1, backgroundColor: '#fff'}}>
-            <ImageBackground source={require('../../../assets/img_login_background.png')}
-                             style={{width: '100%', height: '100%'}} resizeMode='stretch'>
-                <View style={{height: '100%'}}>
-                    <View style={{marginTop: 20, marginLeft: 20}}>
-                        <Text style={{color: "#fff", fontSize: 28, fontWeight: 'bold', marginTop: 10, fontFamily: 'tway_air'}}>Welcome To i-Fresh!</Text>
-                        <Text style={{color: "#fff", fontSize: 15, fontWeight: 'bold', marginTop: 10, fontFamily: 'tway_air'}}>식재료의 신선도와 레시피 확인을
-                            한번에!</Text>
-                    </View>
-                    <View style={{alignItems: 'center', marginTop: 60}}>
-                        <Image source={require('../../../assets/img_login.png')}
-                               style={{width: 150, height: 150}}/>
-                        <Text style={{color: "#24C58B", fontSize: 22, fontWeight: 'bold', fontFamily: 'tway_air'}}>i-Fresh</Text>
-                    </View>
-                    <View style={{marginLeft: 50, marginTop: 40}}>
-                        <Text style={{fontSize: 12, fontFamily: 'tway_air'}}>SNS로 로그인</Text>
-                        <View style={{flexDirection: 'row'}}>
-                            <TouchableOpacity onPress={() => signInWithKakao()}>
-                                <Image source={require('../../../assets/icon_kakao.png')}
-                                   style={{width: 65, height: 65, marginTop: 10}} />
-                            </TouchableOpacity>
-                            <Image source={require('../../../assets/icon_naver.png')}
-                                   style={{width: 65, height: 65, marginTop: 10, marginLeft: 20}}/>
-                        </View>
-                    </View>
-                    <View style={{
-                        width: '100%',
-                        alignItems: 'center',
-                        marginTop: 30,
-                    }}>
-                        <TouchableOpacity style={styles.greenButton} onPress={() => navigation.navigate('Login')}>
-                            <Text style={{color: "#fff", fontWeight: 'bold', fontFamily: 'tway_air'}}>로그인</Text>
-                        </TouchableOpacity>
-                        <TouchableOpacity style={styles.greenButton} onPress={() => navigation.navigate('Register')}>
-                            <Text style={{color: "#fff", fontWeight: 'bold', fontFamily: 'tway_air'}}>회원가입</Text>
-                        </TouchableOpacity>
-                        <TouchableOpacity style={styles.whiteButton} onPress={() => navigation.navigate('SearchID1')}>
-                            <Text style={{color: "#2d2d2d", fontWeight: 'bold', fontFamily: 'tway_air'}}>아이디 찾기</Text>
-                        </TouchableOpacity>
-                        <TouchableOpacity style={styles.whiteButton} onPress={() => navigation.navigate('SearchPW')}>
-                            <Text style={{color: "#2d2d2d", fontWeight: 'bold', fontFamily: 'tway_air'}}>비밀번호 찾기</Text>
-                        </TouchableOpacity>
-                    </View>
-                    <View style={{marginTop: 30}}>
-                        <Text style={{alignSelf: 'center', fontSize: 10, fontFamily: 'tway_air'}}>Copyright HYWU i-Fresh 2021.</Text>
-                    </View>
-                </View>
-            </ImageBackground>
-        </SafeAreaView>
-    );
+  return (
+    <SafeAreaView style={{ flex: 1, backgroundColor: "#fff" }}>
+      <ScrollView>
+        <ImageBackground source={require("../../../assets/img_login_background.png")}
+                         style={{ width: "100%", height: "100%" }} resizeMode="stretch">
+          <View style={{ height: "100%" }}>
+            <View style={{ marginTop: 20, marginLeft: 20 }}>
+              <Text style={{ color: "#fff", fontSize: 28, fontWeight: "bold", marginTop: 10, fontFamily: "tway_air" }}>Welcome
+                To i-Fresh!</Text>
+              <Text style={{ color: "#fff", fontSize: 15, fontWeight: "bold", marginTop: 10, fontFamily: "tway_air" }}>식재료의
+                신선도와 레시피 확인을
+                한번에!</Text>
+            </View>
+            <View style={{ alignItems: "center", justifyContent: "center", marginTop: 100 }}>
+              <Text
+                style={{ color: "#24C58B", fontSize: 22, fontWeight: "bold", fontFamily: "tway_air" }}>i-Fresh</Text>
+            </View>
+            <View style={{
+              width: 300,
+              alignSelf: "center",
+              alignItems: "flex-start",
+              marginTop: 50,
+            }}>
+              <Text style={{ fontFamily: "tway_air", fontWeight: "bold", fontSize: 15 }}>LOGIN</Text>
+              <TextInput style={AuthStyle.input} placeholder={"E-mail"} />
+              <TextInput style={AuthStyle.input} placeholder={"PASSWORD"} />
+              <View style={{
+                width: 300,
+                height: 40,
+                borderRadius: 7,
+                backgroundColor: "#24C58B",
+                marginTop: 10,
+                alignItems: "center",
+                justifyContent: "center",
+              }}>
+                <Text style={{ fontSize: 14, fontWeight: "bold", fontFamily: "tway_air", color: "#fff" }}>로그인</Text>
+              </View>
+              <View style={{ alignSelf: "flex-end", flexDirection: "row", marginTop: 20 }}>
+                <Text style={{ fontFamily: "tway_air", fontSize: 14, color: "#2097F6" }}>아이디 찾기 | </Text>
+                <Text style={{ fontFamily: "tway_air", fontSize: 14, color: "#2097F6" }}>비밀번호 찾기</Text>
+              </View>
+            </View>
+            <View style={{
+              width: 300,
+              alignSelf: "center",
+              alignItems: "flex-start",
+              marginTop: 50,
+              // backgroundColor: "pink",
+            }}>
+              <Text style={{ fontFamily: "tway_air", fontWeight: "bold", fontSize: 15 }}>SNS LOGIN</Text>
+              <View style={{
+                width: 300,
+                height: 40,
+                borderRadius: 7,
+                backgroundColor: "#FEDE00",
+                marginTop: 10,
+                alignItems: "center",
+                justifyContent: "center",
+                flexDirection: "row",
+              }}>
+                <Image source={require("../../../assets/icon_kakao.png")} style={{ width: 40, height: 40 }} />
+                <Text style={{ color: "#3C1E1C", fontFamily: "tway_air", fontSize: 14 }}>카카오톡으로 로그인</Text>
+              </View>
+              <View style={{
+                width: 300,
+                height: 40,
+                borderRadius: 7,
+                backgroundColor: "#1DC800",
+                marginTop: 10,
+                alignItems: "center",
+                justifyContent: "center",
+                flexDirection: "row",
+              }}>
+                <Image source={require("../../../assets/icon_naver.png")} style={{ width: 40, height: 40 }} />
+                <Text style={{ color: "#fff", fontFamily: "tway_air", fontSize: 14 }}>네이버로 로그인</Text>
+              </View>
+              <View style={{ marginTop: 40, alignSelf: "center", flexDirection: "row" }}>
+                <Text style={{ color: "#2d2d2d", fontFamily: "tway_air", fontSize: 14 }}>i-Fresh의 회원이 아니신가요?</Text>
+                <Text style={{ color: "#2097F6", fontFamily: "tway_air", fontSize: 14, marginLeft: 10 }}>회원가입</Text>
+              </View>
+            </View>
+            <View style={{ marginTop: 50 }}>
+              <Text style={{ alignSelf: "center", fontSize: 10, fontFamily: "tway_air" }}>Copyright HYWU i-Fresh
+                2021.</Text>
+            </View>
+          </View>
+        </ImageBackground>
+      </ScrollView>
+    </SafeAreaView>
+  );
 };
 
-const styles = StyleSheet.create({
-    whiteButton: {
-        width: 300,
-        height: 35,
-        alignItems: 'center',
-        justifyContent: 'center',
-        alignSelf: 'center',
-        marginBottom : 20,
-        borderRadius: 5,
-        backgroundColor: "#fff",
-        borderColor: '#DCDBE6',
-        borderWidth: 1
-    },
-    greenButton: {
-        width: 300,
-        height: 35,
-        alignItems: 'center',
-        justifyContent: 'center',
-        alignSelf: 'center',
-        marginBottom : 20,
-        borderRadius: 5,
-        backgroundColor: "#24C58B"
-    }
-});
+const styles = StyleSheet.create({});
 
-export {LoginPage};
+export { LoginPage };
